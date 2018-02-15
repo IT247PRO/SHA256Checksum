27e49885cd523eb84d0519ae0079d5c7843a5de3
mazhar <mazhar@HOOVR3063C092B>
2018-02-15 12:56:22 -0600

    Add project files.

---
+++ SHA256Checksum/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SHA256Checksum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.openFileXML = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblChecksum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 95);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(900, 20);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(837, 36);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Enabled = false;
+            this.txtFileName.Location = new System.Drawing.Point(12, 38);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(819, 20);
+            this.txtFileName.TabIndex = 2;
+            // 
+            // lblChecksum
+            // 
+            this.lblChecksum.AutoSize = true;
+            this.lblChecksum.ForeColor = System.Drawing.Color.Red;
+            this.lblChecksum.Location = new System.Drawing.Point(13, 69);
+            this.lblChecksum.Name = "lblChecksum";
+            this.lblChecksum.Size = new System.Drawing.Size(0, 13);
+            this.lblChecksum.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 127);
+            this.Controls.Add(this.lblChecksum);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "SHA 256 Checksum Genrator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.OpenFileDialog openFileXML;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblChecksum;
+    }
+}
+
---
+++ SHA256Checksum/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SHA256Checksum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (this.openFileXML.ShowDialog() != DialogResult.OK)
+                return;
+            try
+            {
+                this.txtFileName.Text = this.openFileXML.FileName;
+                this.lblChecksum.Text = @"MD5 Checksum for: " + Path.GetFileName(this.openFileXML.FileName);
+                this.txtResult.Text = this.CalculateChecksum(this.openFileXML.FileName);
+            }
+            catch (Exception ex)
+            {
+                this.txtResult.Text = ex.Message;
+            }
+        }
+
+        private string CalculateChecksum(string filename)
+        {
+            using (FileStream fileStream = File.OpenRead(filename))
+                return GetSha512Buffered(fileStream);
+        }
+
+
+        private static string GetSha512Buffered(Stream p_streamIn)
+        {
+            string _result;
+
+            Process.GetCurrentProcess();
+            const int _bufferSizeForMd5Hash = 1024 * 1024 * 8;
+
+            using (var _md5Prov = new SHA256Managed())
+            {
+                int _readCount;
+                var _bytesTransfered = 0;
+                var _buffer = new byte[_bufferSizeForMd5Hash];
+
+                while ((_readCount = p_streamIn.Read(_buffer, 0, _buffer.Length)) != 0)
+                {
+                    if (_bytesTransfered + _readCount == p_streamIn.Length)
+                    {
+                        _md5Prov.TransformFinalBlock(_buffer, 0, _readCount);
+                    }
+                    else
+                    {
+                        _md5Prov.TransformBlock(_buffer, 0, _bufferSizeForMd5Hash, _buffer, 0);
+                    }
+                    _bytesTransfered += _readCount;
+                }
+
+                _result = BitConverter.ToString(_md5Prov.Hash).Replace("-", String.Empty).ToLower();
+                _md5Prov.Clear();
+            }
+
+            return _result;
+        }
+
+        private static string GetSha512BufferedStream(Stream p_stream)
+        {
+            var _bufferedStream = new BufferedStream(p_stream, 1024 * 1024 * 8);
+            var _sha256 = new SHA256Managed();
+
+            var _checksum = _sha256.ComputeHash(_bufferedStream);
+
+            var _process = Process.GetCurrentProcess();
+            return BitConverter.ToString(_checksum).Replace("-", String.Empty);
+        }
+    }
+}
